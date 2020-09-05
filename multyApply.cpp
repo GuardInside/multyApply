4d4c35b69a217fb0753743e7200de05fccc56052
Viktor Leonov <48095293+GuardInside@users.noreply.github.com>
2020-09-06 02:18:19 +0300

    Update multyApply.cpp

--- multyApply.cpp
+++ multyApply.cpp
@@ -4,20 +4,16 @@ using namespace std;
 #include <functional>
 #include <tuple>
 
-
-namespace detail
+template <class F, class Tuple, std::size_t... I>
+constexpr void multyApply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
 {
-    template <class F, class Tuple, std::size_t... I>
-    constexpr void multyApply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
-    {
-        ((std::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t)))),...);
-    }
+    ((std::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t)))),...);
 }
 
 template <class F, class Tuple>
 constexpr decltype(auto) multyApply(F&& f, Tuple&& t)
 {
-    detail::multyApply_impl(
+    multyApply_impl(
         std::forward<F>(f),
         std::forward<Tuple>(t),
         std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{});
